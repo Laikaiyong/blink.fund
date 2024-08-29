782976ffc60ea056693b702906b73887ddcb623c
Vandyck <vandycklai@gmail.com>
2024-08-30 02:50:32 +0800

    Hotfix

---
+++ src/app/api/get-blink/route.js
@@ -0,0 +1,28 @@
+import {
+    ActionPostResponse,
+    createActionHeaders,
+    createPostResponse,
+    ActionGetResponse,
+    ActionPostRequest,
+  } from "@solana/actions";
+  
+  export const GET = async () => {
+        const transaction = {};
+      // insert transaction logic here
+      const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Encoding, Accept-Encoding',
+      }  
+          
+      const payload = await createPostResponse({
+        fields: {
+          transaction,
+          message: "Optional message to include with transaction",
+        },
+      });
+      
+      return Response.json(payload, {
+        headers,
+      });
+    };
