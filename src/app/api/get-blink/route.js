320cae6c47fb6e03f366bfc2b272c69ef582864a
Vandyck <vandycklai@gmail.com>
2024-08-30 02:58:15 +0800

    JSON return get request

--- src/app/api/get-blink/route.js
+++ src/app/api/get-blink/route.js
@@ -7,22 +7,8 @@ import {
   } from "@solana/actions";
   
   export async function GET() {
-        const transaction = {};
-      // insert transaction logic here
-      const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,POST,PUT,OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Encoding, Accept-Encoding',
-      }  
-          
-      const payload = await createPostResponse({
-        fields: {
-          transaction,
-          message: "Optional message to include with transaction",
-        },
-      });
       
-      return Response.json(payload, {
-        headers,
-      });
-    };
+    return Response.json({
+        "status": "Error"
+    });
+    }
