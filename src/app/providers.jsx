7f93523d13150eb4881aa96bf4714a17fef7fb80
Vandyck <vandycklai@gmail.com>
2024-08-30 03:48:43 +0800

    Dependencies wallet

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -7,34 +7,10 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-  SkyWalletAdapter,
-  MathWalletAdapter,
-  NekoWalletAdapter,
-  NufiWalletAdapter,
-  OntoWalletAdapter,
-  SpotWalletAdapter,
-  AlphaWalletAdapter,
-  AvanaWalletAdapter,
-  HuobiWalletAdapter,
-  SaifuWalletAdapter,
-  TorusWalletAdapter,
-  PhantomWalletAdapter,
-  TrustWalletAdapter,
-  BitgetWalletAdapter,
-  BitpieWalletAdapter,
-  CloverWalletAdapter,
-  Coin98WalletAdapter,
-  CoinhubWalletAdapter,
-  LedgerWalletAdapter,
-  SolongWalletAdapter,
-  SolflareWalletAdapter,
-  NightlyWalletAdapter,
-  SafePalWalletAdapter,
-  SalmonWalletAdapter,
-  TokenPocketWalletAdapter,
-  TokenaryWalletAdapter
-} from "@solana/wallet-adapter-wallets";
+import { Coin98WalletAdapter } from "@solana/wallet-adapter-coin98";
+import { CoinbaseWalletAdapter } from "@solana/wallet-adapter-coinbase";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
 import {
   WalletModalProvider,
   WalletDisconnectButton,
@@ -45,9 +21,7 @@ import { clusterApiUrl } from "@solana/web3.js";
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
 
-export const WalletProviders = ({
-  children,
-}) => {
+export const WalletProviders = ({ children }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
   const network = WalletAdapterNetwork.Devnet;
 
@@ -68,46 +42,23 @@ export const WalletProviders = ({
        * instantiate its legacy wallet adapter here. Common legacy adapters can be found
        * in the npm package `@solana/wallet-adapter-wallets`.
        */
-      new SkyWalletAdapter(),
-      new MathWalletAdapter(),
-      new NekoWalletAdapter(),
-      new NufiWalletAdapter(),
-      new OntoWalletAdapter(),
-      new SpotWalletAdapter(),
-      new AlphaWalletAdapter(),
-      new AvanaWalletAdapter(),
-      new HuobiWalletAdapter(),
-      new SaifuWalletAdapter(),
-      new TorusWalletAdapter(),
+
       new PhantomWalletAdapter(),
-      new TrustWalletAdapter(),
-      new BitgetWalletAdapter(),
-      new BitpieWalletAdapter(),
-      new CloverWalletAdapter(),
       new Coin98WalletAdapter(),
-      new CoinhubWalletAdapter(),
-      new LedgerWalletAdapter(),
-      new SolongWalletAdapter(),
+      new CoinbaseWalletAdapter(),
       new SolflareWalletAdapter(),
-      new NightlyWalletAdapter(),
-      new SafePalWalletAdapter(),
-      new SalmonWalletAdapter(),
-      new TokenPocketWalletAdapter(),
-      new TokenaryWalletAdapter(),
     ],
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [network]
   );
 
   return (
-          <ThemeProvider>
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          {children}
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-          </ThemeProvider>
+    <ThemeProvider>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>{children}</WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </ThemeProvider>
   );
 };
